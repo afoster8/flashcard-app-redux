ce95854e874015ac28b4af6c264f4fcfd57653ef
afoster8 <afoster8@umd.edu>
2024-01-01 02:22:42 -0500

    deploying

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/auth", authRoutes);
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test');
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
